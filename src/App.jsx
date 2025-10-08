de3392037d71a6b8d3029922f23cd3530422faca
SpandanaKona17 <SpandanaKona017@gmail.com>
2025-10-08 18:20:12 +0530

    Initial commit - Aspire project setup

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Footer from './components/Footer';
+import { CartProvider } from './context/CartContext';
+import './App.css';
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<ProductList />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
